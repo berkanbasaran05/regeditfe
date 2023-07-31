7d5642b561f9495615c3f31b8c2163bb4c32e7d1
berkanbasaran05 <berkanbasaran05@gmail.com>
2023-07-31 19:22:46 +0300

    last

--- src/Pages/Preloader/Preloader.js
+++ src/Pages/Preloader/Preloader.js
@@ -3,10 +3,14 @@ import React from 'react';
 const Preloader = () => {
     return (
         <div className="theme-loader">
+            
             <div className="spinner">
+            <img src='assets/img/logo-1.png'></img>
+           
                 <div className="double-bounce1"></div>
                 <div className="double-bounce2"></div>
             </div>
+          
         </div>
     );
 };
